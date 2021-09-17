9022797f1517f9bfcc42985f5733d58002d38fb7
GittyHoo <90148289+GittyHoo@users.noreply.github.com>
2021-09-17 14:24:26 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,13 +1,10 @@
-console.log ("hello")
+console.log("hello")
 
-const menuBtn = document.querySelector('#menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
-  }
-});
+var hamburgerMenu = document.querySelector(".hamburger");
+
+function hamburgerOpen(){
+    let Hamburger = document.querySelector("nav");
+    Hamburger.classList.toggle("hamburgerOpen");
+}
+
+hamburgerMenu.addEventListener("click", hamburgerOpen);
